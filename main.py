1c5139d2e7cc79016bd6157a4dd111e7d3442ec2
Andrea Dal Pozzolo <andrea.dalpozzolo@pmi.com>
2018-04-16 15:01:19 +0200

    add evaluation

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from csv_reader import CsvReader
 from DataScientistMethod import train_split_data
 from model import IrisModel
+import sys
+
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
+
 
 #import os
 #dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -17,9 +22,16 @@ test_data, to_learn = train_split_data(data, 'iris_type', 0.3)
 irisModel = IrisModel(test_data, to_learn, data.get_types_of_data('iris_type'))
 
 irisModel.learn()
+
+"""
 for item in test_data:
     a = irisModel.predict(item)
-    b = 1
+    max_prob = max([x[0] for x in a])
+    pred_class = [y[1] for y in a if y[0]==max_prob]
+"""
+
+irisModel.evalution(test_data)
+
 
 
 
