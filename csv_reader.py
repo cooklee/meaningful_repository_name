75414f9437852588ab42c563aa25440e9f2c368b
Alvaro Rabadan <alvaro.rabadan@pmi.com>
2018-04-16 14:23:41 +0200

    ✨  Read

--- csv_reader.py
+++ csv_reader.py
@@ -5,21 +5,22 @@ class CsvReader(object):
 
     @staticmethod
     def create_from_csv(csv_path, sep=";"):
-        data_read = CsvReader.__read_from_file(csv_path)
-        unprocessed_data = CsvReader.__create_data_frame_from_read_data(data_read, sep=sep)
+        data_read = CsvReader.read_from_file(csv_path)
+        unprocessed_data = CsvReader.create_data_frame_from_read_data(data_read, sep=sep)
         return DataFrame(labels=CsvReader.create_labels(unprocessed_data),
                          data=CsvReader.create_data(unprocessed_data))
 
     @staticmethod
-    def __read_from_file(file_path):
+    def read_from_file(file_path):
         with open(file_path, 'r') as file:
             readlines = []
             for line in file.readlines():
+                line = line.replace(',', '.')
                 readlines.append(line[0:-1])
             return readlines
 
     @staticmethod
-    def __create_data_frame_from_read_data(read_lines, sep=";"):
+    def create_data_frame_from_read_data(read_lines, sep=";"):
         divided_data = [line.split(sep) for line in read_lines]
         return divided_data
 
--- main.py
+++ main.py
@@ -14,12 +14,12 @@ data = CsvReader.create_from_csv("iris.csv",sep=";")
 test_data, to_learn = train_split_data(data, 'iris_type', 0.3)
 
 irisModel = IrisModel(test_data, to_learn, data.get_types_of_data('iris_type'))
-
 irisModel.learn()
+
 for item in test_data:
+    print(item)
     a = irisModel.predict(item)
-    b =1
-#
+    print(a)
 
 
 
