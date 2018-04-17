c018e7e68c5042bad85b4f063f7ef780010da5ff
Andrea Dal Pozzolo <andrea.dalpozzolo@pmi.com>
2018-04-17 14:00:29 +0200

    fixed command interface

--- main.py
+++ main.py
@@ -10,28 +10,33 @@ import os
 args = sys.argv
 n_args = len(args)
 
+if (n_args < 4):
+    output = 1
+else :
+    output = 2
+    
+    
 #print ('Number of arguments:', n_args, 'arguments.')
 #print ('Argument List:', str(args))
 
-file_path = args[1]
-sepal_length = args[2]
-sepal_width = args[3]
-petal_length = args[4]
-petal_width = args[5]
+if (output == 1):
+    # new file to load
+    file_path = args[2] 
+    # load data
+    data = CsvReader.create_from_csv(file_path, sep=";")
+else:
+    sepal_length = args[1]
+    sepal_width = args[2]
+    petal_length = args[3]
+    petal_width = args[4]
+    # load data
+    data = CsvReader.create_from_csv("iris.csv",sep=";")
 
 
-if(n_args == 7):
-    optinal_file = args[6]
-    print(optinal_file)
-
-
+    
 
 #dir_path = os.path.dirname(os.path.realpath(file_path))
 #print("!!!!!!"+dir_path)
-
-# load data
-data = CsvReader.create_from_csv(file_path, sep=";")
-#data = CsvReader.create_from_csv("iris.csv",sep=";")
 
 
 # data preparation
@@ -42,32 +47,21 @@ irisModel = IrisModel(test_data, to_learn, data.get_types_of_data('iris_type'))
 irisModel.learn()
 
 
-item_list = [sepal_length, sepal_width, petal_length, petal_width]
-item = [float(i) for i in item_list]
-a = irisModel.predict(item)
-max_prob = max([x[0] for x in a])
-pred_class = [y[1] for y in a if y[0]==max_prob][0]
+if (output == 1):
+    print("")
+    irisModel.evalution(test_data)
 
-print("")
-print("Input file:", file_path)
-print ('Input values: sepal length', sepal_length, 'sepal width', sepal_width, 
-       "petal length", petal_length, "petal width", petal_width)
-print("Output class:", pred_class)
-
-    
-"""
-for item in test_data:
+else:
+    item_list = [sepal_length, sepal_width, petal_length, petal_width]
+    item = [float(i) for i in item_list]
     a = irisModel.predict(item)
     max_prob = max([x[0] for x in a])
-    pred_class = [y[1] for y in a if y[0]==max_prob]
-"""
+    pred_class = [y[1] for y in a if y[0]==max_prob][0]
 
-#irisModel.evalution(test_data)
-
-
-
-
-
+    print("")
+    print ('Input values: sepal length', sepal_length, 'sepal width', sepal_width, 
+           "petal length", petal_length, "petal width", petal_width)
+    print("Output class:", pred_class)
 
 
 
