2a4f04d12c3c1ca9f1b5c56ee0e59fa6ca09aee5
shivampahuja <shivam.pahuja@pmi.com>
2018-04-17 14:27:32 +0200

    update

--- README.md
+++ README.md
@@ -3,6 +3,22 @@
 ```
 $ pip install coverage
 $ coverage run --source=./  -m pytest
-$ ccoverage report -m --omit=main.py,tests/*
+$ coverage report -m --omit=main.py,tests/*
+
+```
+
+
+### usage
+```
+$$ python main.py -i='predict' -v 4 2 1 0
+('Inputted values', [4, 2, 1, 0])
+[(0.1875, 'setosa'), (0.0625, 'versicolor'), (0, 'virginica')]
+PREDICTED TYPE : setosa
+
+
+
+$python main.py -i='evaluate' -f='predict.csv'
+ACCURACY : 0.88
+
 
 ```
--- csv_reader.py
+++ csv_reader.py
@@ -2,13 +2,12 @@ from DataFrame import DataFrame
 
 
 class CsvReader(object):
-
     @staticmethod
-    def create_from_csv(csv_path, sep=";"):
+    def create_from_csv(csv_path, sep=";", delim=','):
         data_read = CsvReader.__read_from_file(csv_path)
         unprocessed_data = CsvReader.__create_data_frame_from_read_data(data_read, sep=sep)
         return DataFrame(labels=CsvReader.create_labels(unprocessed_data),
-                         data=CsvReader.create_data(unprocessed_data))
+                         data=CsvReader.create_data(unprocessed_data, delim=delim))
 
     @staticmethod
     def __read_from_file(file_path):
@@ -25,11 +24,14 @@ class CsvReader(object):
 
     @staticmethod
     def create_labels(unprocessed_data):
-
         return [x.strip() for x in unprocessed_data[0]]
 
     @staticmethod
-    def create_data(unprocessed_data):
+    def replace_decimal(line, delim=','):
+        line = [el.replace(delim, '.') for el in line]
+        return (line)
+
+    @staticmethod
+    def create_data(unprocessed_data, delim=','):
+        unprocessed_data = [CsvReader.replace_decimal(line, delim=delim) for line in unprocessed_data]
         return unprocessed_data[1:]
-
-
